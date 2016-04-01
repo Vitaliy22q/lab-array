4aa83c3b71e69fc882206e98bef006d9a288d481
Vitaliy22q <Vitaliy22q@list.ru>
2016-04-01 10:53:33 +0300

    Added files via upload

---
+++ zavdannja b.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int mas[100],a,b,i;
+	printf("vvedite b:");
+	scanf("%d",&b);
+	for(i=a;i<b;i++){
+	mas[i]=i+1;
+	printf("\nmas[%d]=%d",i,mas[i]);	
+		
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
