8fba1aabf9a545b6b48d24b02b1f0ac7515ece10
Vitaliy22q <Vitaliy22q@list.ru>
2016-04-01 10:48:31 +0300

    Added files via upload

---
+++ 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int mas[100],i,n,zn;
+	printf("vvedite n=");
+	scanf("%d",&n);
+	for(i=0,zn=0;i<=n;zn+=2,i++){
+		mas[i]=zn;
+		
+		
+		
+	}
+	for (i=0;i<=n;i++){
+		printf("\n zn massiva:%d",mas[i]);
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}
