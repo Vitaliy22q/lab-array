863874bad9ff1c680673e469b2c599281d62459d
Vitaliy22q <Vitaliy22q@list.ru>
2016-05-20 08:41:31 +0300

    Add files via upload
    цілих від -100 до 100

---
+++ 100 d0 100.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int mas[300];
+	int i;
+	srand (time(0));
+	for(i=0;i < 100;i++){
+		mas [i] = (rand () % 200)-100;
+		printf ("\nmas[%d]=%d",i,mas[i]);
+	}
+	
+	return 0;
+}
