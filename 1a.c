fc75aef0b4997ffdc228e4c5c3870f1bae002493
Vitaliy22q <Vitaliy22q@list.ru>
2016-05-20 08:29:41 +0300

    Add files via upload

---
+++ 1a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int mas[300];
+	int i,n;
+	printf("vvedite n:");
+	scanf("%d",&n);
+	srand (time(0));
+	for(i=0;i < n;i++){
+		mas [i] = rand () % 100;
+		printf ("\nmas[%d]=%d",i,mas[i]);
+	}
+	
+	return 0;
+}
